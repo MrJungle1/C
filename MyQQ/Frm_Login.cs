5ca1b62b51d38571d23918232ba93ae7edaf18bd
MrJungle <1505396805@qq.com>
2021-01-09 20:12:25 +0800

    C#+SQLsever

---
+++ MyQQ/Frm_Login.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace MyQQ
+{
+    public partial class Frm_Login : Form
+    {
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+        
+        DataOperator dataOper = new DataOperator();//创建数据操作类的对象
+
+        //注册账号
+        private void linklblReg_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Frm_Register frmRegister = new Frm_Register();//创建申请账号窗体对象
+            frmRegister.Show();//显示申请账号窗体
+        }
+
+        /// <summary>
+        /// 验证用户输入
+        /// </summary>
+        /// <returns>验证成功，返回true，否则返回false</returns>
+        private bool ValidateInput()
+        {
+            if (txtID.Text.Trim() == "")//登录账号
+            {
+                MessageBox.Show("请输入登录账号", "登录提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtID.Focus();//使登录账号文本框获得鼠标焦点
+                return false;
+            }
+            else if(int.Parse(txtID.Text.Trim())>65535)
+            {
+                MessageBox.Show("请输入正确的登录账号", "登录提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtID.Focus();//使登录账号文本框获得鼠标焦点
+                return false;
+            }
+            else if (txtID.Text.Length>5 && txtPwd.Text.Trim() == "") //密码
+            {
+                MessageBox.Show("请输入密码", "登录提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtPwd.Focus();//使密码文本框获得鼠标焦点
+                return false;
+            }
+            return true;
+        }
+
+        //是否忘记密码或者自动登录
+        private void txtID_TextChanged(object sender, EventArgs e)
+        {
+            ValidateInput();
+            //根据号码查询其密码、记住密码和自动登录字段的值
+            string sql = "select Pwd,Remember,AutoLogin from tb_User where ID=" + int.Parse(txtID.Text.Trim()) + "";
+            DataSet ds = dataOper.GetDataSet(sql);//查询结果存储到数据集中
+            if (ds.Tables[0].Rows.Count > 0)  //判断是否存在该用户
+            {
+                if (Convert.ToInt32(ds.Tables[0].Rows[0][1]) == 1)//判断是否记住密码
+                {
+                    cboxRemember.Checked = true;//记录密码复选框选中
+                    txtPwd.Text = ds.Tables[0].Rows[0][0].ToString();//自动输入密码
+                    if (Convert.ToInt32(ds.Tables[0].Rows[0][2]) == 1)//判断是否自动登录
+                    {
+                        cboxAutoLogin.Checked = true;//自动登录复选框选中
+                        pboxLogin_Click(sender, e);//自动登录
+                    }
+                }
+            }
+        }
+
+        //判断是否在用户账号文本框中按下回车键
+        private void txtID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) || (e.KeyChar == '\r') || (e.KeyChar == '\b'))//判断是否为数字
+                e.Handled = false;//在控件中显示该字符
+            else
+                e.Handled = true;//取消在控件中显示该字符
+        }
+
+        //判断是否在密码文本框中按下回车键
+        private void txtPwd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')//判断是否按下回车键
+                pboxLogin_Click(sender, e);//使登录按钮获得鼠标焦点
+        }
+
+        //在记住密码的前提下才可以自动登录
+        private void cboxRemember_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!cboxRemember.Checked)//判断忘记密码文本框是否为未选中状态
+                cboxAutoLogin.Checked = false;//自动登录设置为未选中
+        }
+
+        //登录按钮
+        private void pboxLogin_Click(object sender, EventArgs e)
+        {
+            if (ValidateInput()) //调用自定义方法验证用户输入
+            {
+                //定义查询SQL语句
+                string sql = "select count(*) from tb_User where ID=" + int.Parse(txtID.Text.Trim()) + " and Pwd = '" + txtPwd.Text.Trim() + "'";
+                int num = dataOper.ExecSQL(sql);
+                if (num == 1)//验证通过
+                {
+                    PublicClass.loginID = int.Parse(txtID.Text.Trim());//设置登录的用户号码
+                    if (cboxRemember.Checked)
+                    {
+                        //记住密码
+                        dataOper.ExecSQLResult("update tb_User set Remember=1 where ID=" + int.Parse(txtID.Text.Trim()));
+                        if (cboxAutoLogin.Checked)
+                            //自动登录
+                            dataOper.ExecSQLResult("update tb_User set AutoLogin=1 where ID=" + int.Parse(txtID.Text.Trim()));
+                    }
+                    else
+                    {
+                        dataOper.ExecSQLResult("update tb_User set Remember=0 where ID=" + int.Parse(txtID.Text.Trim()));
+                        dataOper.ExecSQLResult("update tb_User set AutoLogin=0 where ID=" + int.Parse(txtID.Text.Trim()));
+                    }
+                    dataOper.ExecSQLResult("update tb_User set Flag=1 where ID=" + int.Parse(txtID.Text.Trim()));//设置在线状态
+                    Frm_Main frmMain = new Frm_Main();//创建主窗体对象
+                    frmMain.Show();  //显示主窗体
+                    this.Visible = false;  //隐藏登录主窗体
+                }
+                else
+                {
+                    MessageBox.Show("输入的用户名或密码有误！", "登录提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        //最小化窗体
+        private void pboxMin_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;//设置窗体最小化
+        }
+
+        //退出程序
+        private void pboxClose_Click(object sender, EventArgs e)
+        {
+            Application.ExitThread();//退出当前应用程序
+        }
+
+        //拖动窗体
+        private void Frm_Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            PublicClass.ReleaseCapture();//用来释放被当前线程中某个窗口捕获的光标
+            PublicClass.SendMessage(this.Handle, PublicClass.WM_SYSCOMMAND, PublicClass.SC_MOVE + PublicClass.HTCAPTION, 0);//向Windows发送拖动窗体的消息
+        }
+    }
+}
---
+++ MyQQ/Frm_Register.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace MyQQ
+{
+    partial class Frm_Register
+    {
+        ///<summary>
+        ///必需的设计器变量。
+        ///</summary>
+        private System.ComponentModel.IContainer components = null;
+
+        ///<summary>
+        ///清理所有正在使用的资源。
+        ///</summary>
+        ///<param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        ///<summary>
+        ///设计器支持所需的方法 - 不要
+        ///使用代码编辑器修改此方法的内容。
+        ///</summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Register));
+            this.grpBaseInfo = new System.Windows.Forms.GroupBox();
+            this.txtPwdAgain = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtNickName = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblPwdAgain = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.lblNickName = new System.Windows.Forms.Label();
+            this.grpDetails = new System.Windows.Forms.GroupBox();
+            this.cboxBloodType = new System.Windows.Forms.ComboBox();
+            this.cboxStar = new System.Windows.Forms.ComboBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblBloodType = new System.Windows.Forms.Label();
+            this.lblStar = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.grpBaseInfo.SuspendLayout();
+            this.grpDetails.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpBaseInfo
+            // 
+            this.grpBaseInfo.BackColor = System.Drawing.Color.Transparent;
+            this.grpBaseInfo.Controls.Add(this.rbtnFemale);
+            this.grpBaseInfo.Controls.Add(this.rbtnMale);
+            this.grpBaseInfo.Controls.Add(this.txtPwdAgain);
+            this.grpBaseInfo.Controls.Add(this.txtPwd);
+            this.grpBaseInfo.Controls.Add(this.txtAge);
+            this.grpBaseInfo.Controls.Add(this.txtNickName);
+            this.grpBaseInfo.Controls.Add(this.lblAge);
+            this.grpBaseInfo.Controls.Add(this.lblPwdAgain);
+            this.grpBaseInfo.Controls.Add(this.lblPwd);
+            this.grpBaseInfo.Controls.Add(this.lblSex);
+            this.grpBaseInfo.Controls.Add(this.lblNickName);
+            this.grpBaseInfo.Location = new System.Drawing.Point(114, 10);
+            this.grpBaseInfo.Name = "grpBaseInfo";
+            this.grpBaseInfo.Size = new System.Drawing.Size(259, 180);
+            this.grpBaseInfo.TabIndex = 0;
+            this.grpBaseInfo.TabStop = false;
+            this.grpBaseInfo.Text = "注册基本资料";
+            // 
+            // txtPwdAgain
+            // 
+            this.txtPwdAgain.Location = new System.Drawing.Point(82, 143);
+            this.txtPwdAgain.Name = "txtPwdAgain";
+            this.txtPwdAgain.PasswordChar = '*';
+            this.txtPwdAgain.Size = new System.Drawing.Size(142, 21);
+            this.txtPwdAgain.TabIndex = 9;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(82, 115);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(142, 21);
+            this.txtPwd.TabIndex = 8;
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Location = new System.Drawing.Point(133, 85);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(35, 16);
+            this.rbtnFemale.TabIndex = 1;
+            this.rbtnFemale.Text = "女";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Checked = true;
+            this.rbtnMale.Location = new System.Drawing.Point(82, 85);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(35, 16);
+            this.rbtnMale.TabIndex = 0;
+            this.rbtnMale.Text = "男";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(82, 56);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(42, 21);
+            this.txtAge.TabIndex = 6;
+            // 
+            // txtNickName
+            // 
+            this.txtNickName.Location = new System.Drawing.Point(82, 26);
+            this.txtNickName.Name = "txtNickName";
+            this.txtNickName.Size = new System.Drawing.Size(142, 21);
+            this.txtNickName.TabIndex = 5;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(47, 60);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(29, 12);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "年龄";
+            // 
+            // lblPwdAgain
+            // 
+            this.lblPwdAgain.AutoSize = true;
+            this.lblPwdAgain.Location = new System.Drawing.Point(23, 147);
+            this.lblPwdAgain.Name = "lblPwdAgain";
+            this.lblPwdAgain.Size = new System.Drawing.Size(53, 12);
+            this.lblPwdAgain.TabIndex = 3;
+            this.lblPwdAgain.Text = "重复密码";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(47, 119);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(29, 12);
+            this.lblPwd.TabIndex = 2;
+            this.lblPwd.Text = "密码";
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(47, 87);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(29, 12);
+            this.lblSex.TabIndex = 1;
+            this.lblSex.Text = "性别";
+            // 
+            // lblNickName
+            // 
+            this.lblNickName.AutoSize = true;
+            this.lblNickName.Location = new System.Drawing.Point(47, 30);
+            this.lblNickName.Name = "lblNickName";
+            this.lblNickName.Size = new System.Drawing.Size(29, 12);
+            this.lblNickName.TabIndex = 0;
+            this.lblNickName.Text = "昵称";
+            // 
+            // grpDetails
+            // 
+            this.grpDetails.BackColor = System.Drawing.Color.Transparent;
+            this.grpDetails.Controls.Add(this.cboxBloodType);
+            this.grpDetails.Controls.Add(this.cboxStar);
+            this.grpDetails.Controls.Add(this.txtName);
+            this.grpDetails.Controls.Add(this.lblBloodType);
+            this.grpDetails.Controls.Add(this.lblStar);
+            this.grpDetails.Controls.Add(this.lblName);
+            this.grpDetails.Location = new System.Drawing.Point(114, 206);
+            this.grpDetails.Name = "grpDetails";
+            this.grpDetails.Size = new System.Drawing.Size(259, 116);
+            this.grpDetails.TabIndex = 1;
+            this.grpDetails.TabStop = false;
+            this.grpDetails.Text = "选填详细资料";
+            // 
+            // cboxBloodType
+            // 
+            this.cboxBloodType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxBloodType.FormattingEnabled = true;
+            this.cboxBloodType.Items.AddRange(new object[] {
+            "A型",
+            "B型",
+            "O型",
+            "AB型"});
+            this.cboxBloodType.Location = new System.Drawing.Point(82, 79);
+            this.cboxBloodType.Name = "cboxBloodType";
+            this.cboxBloodType.Size = new System.Drawing.Size(142, 20);
+            this.cboxBloodType.TabIndex = 7;
+            // 
+            // cboxStar
+            // 
+            this.cboxStar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxStar.FormattingEnabled = true;
+            this.cboxStar.Items.AddRange(new object[] {
+            "白羊座",
+            "金牛座",
+            "双子座",
+            "巨蟹座",
+            "狮子座",
+            "处女座",
+            "天秤座",
+            "天蝎座",
+            "射手座",
+            "摩羯座",
+            "水瓶座",
+            "双鱼座"});
+            this.cboxStar.Location = new System.Drawing.Point(82, 50);
+            this.cboxStar.Name = "cboxStar";
+            this.cboxStar.Size = new System.Drawing.Size(142, 20);
+            this.cboxStar.TabIndex = 6;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(82, 22);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(142, 21);
+            this.txtName.TabIndex = 3;
+            // 
+            // lblBloodType
+            // 
+            this.lblBloodType.AutoSize = true;
+            this.lblBloodType.Location = new System.Drawing.Point(47, 82);
+            this.lblBloodType.Name = "lblBloodType";
+            this.lblBloodType.Size = new System.Drawing.Size(29, 12);
+            this.lblBloodType.TabIndex = 2;
+            this.lblBloodType.Text = "血型";
+            // 
+            // lblStar
+            // 
+            this.lblStar.AutoSize = true;
+            this.lblStar.Location = new System.Drawing.Point(47, 54);
+            this.lblStar.Name = "lblStar";
+            this.lblStar.Size = new System.Drawing.Size(29, 12);
+            this.lblStar.TabIndex = 1;
+            this.lblStar.Text = "星座";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(23, 26);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(53, 12);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "真实姓名";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.SystemColors.Control;
+            this.btnRegister.Location = new System.Drawing.Point(234, 326);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(63, 23);
+            this.btnRegister.TabIndex = 2;
+            this.btnRegister.Text = "注册";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.Control;
+            this.btnCancel.Location = new System.Drawing.Point(303, 326);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(70, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackColor = System.Drawing.Color.Transparent;
+            this.picLogo.Image = global::MyQQ.Properties.Resources.Logo;
+            this.picLogo.Location = new System.Drawing.Point(12, 12);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(95, 310);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 4;
+            this.picLogo.TabStop = false;
+            // 
+            // Frm_Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(386, 358);
+            this.Controls.Add(this.picLogo);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.grpDetails);
+            this.Controls.Add(this.grpBaseInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Frm_Register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "申请账号";
+            this.Load += new System.EventHandler(this.Frm_Register_Load);
+            this.grpBaseInfo.ResumeLayout(false);
+            this.grpBaseInfo.PerformLayout();
+            this.grpDetails.ResumeLayout(false);
+            this.grpDetails.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpBaseInfo;
+        private System.Windows.Forms.Label lblNickName;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblPwdAgain;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtNickName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.TextBox txtPwdAgain;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.GroupBox grpDetails;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblStar;
+        private System.Windows.Forms.Label lblBloodType;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cboxStar;
+        private System.Windows.Forms.ComboBox cboxBloodType;
+        private System.Windows.Forms.PictureBox picLogo;
+    }
+}
